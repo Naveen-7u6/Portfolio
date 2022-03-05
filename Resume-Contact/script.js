d5ab878b95ebd1d161fe2548c5d3cc98a9d14a48
Naveen Kumar S <74402250+Naveen-7u6@users.noreply.github.com>
2022-03-05 17:18:15 +0530

    Updation Success - 1

---
+++ Resume-Contact/script.js
@@ -0,0 +1,15 @@
+function sendMail(oarams){
+    
+    var temParams = {
+        name:document.getElementById("name").value,
+        email:document.getElementById("email").value,
+        contact:document.getElementById("contact").value,
+        message:document.getElementById("msg").value,
+    };
+ 
+    emailjs.send('service_ahkbo0i','template_duvtq4r',temParams)
+    .then(function(res){
+        console.log("success",res.status);
+    })
+ 
+ }
